b443262c707ca9621a34c8fe6b6c7adf6a201112
Carlos Estrada <trokotech.cuentas@icloud.com>
2020-06-06 00:46:12 +0200

    mY default

--- themes/Molokai.js
+++ themes/Molokai.js
@@ -1,4 +1,10 @@
-t.prefs_.set('color-palette-overrides',["#121212", "#fa2573", "#98e123", "#dfd460", "#1080d0", "#8700ff", "#43a8d0", "#bbbbbb", "#555555", "#f6669d", "#b1e05f", "#fff26d", "#00afff", "#af87ff", "#51ceff", "#ffffff"]);
+// t.prefs_.set('color-palette-overrides',["#121212", "#fa2573", "#98e123", "#dfd460", "#1080d0", "#8700ff", "#43a8d0", "#bbbbbb", "#555555", "#f6669d", "#b1e05f", "#fff26d", "#00afff", "#af87ff", "#51ceff", "#ffffff"]);
+// t.prefs_.set('foreground-color', "#ffffff");
+// t.prefs_.set('background-color', "#000000");
+// t.prefs_.set('cursor-color', 'rgba(63, 222, 233, 0.5)');
+
+// t.prefs_.resetAll();
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#000000");
-t.prefs_.set('cursor-color', 'rgba(63, 222, 233, 0.5)');
+term_set('cursor-color', 'rgba(63, 222, 233, 0.5)');
+term_set('cursor-blink', true);
