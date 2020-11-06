465ade2b5e585e68690169fbe10e2b38141ce605
Carlos Estrada <trokotech@gmail.com>
2020-11-06 17:41:38 +0100

    alphaa to .8

--- themes/MyDefault.js
+++ themes/MyDefault.js
@@ -1,4 +1,4 @@
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#000000");
-term_set('cursor-color', 'rgba(255, 255, 255, 1.0)');
+term_set('cursor-color', 'rgba(255, 255, 255, 0.8 )');
 term_set('cursor-blink', true);
