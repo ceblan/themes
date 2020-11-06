709d407994709c845f537d4f1ab09a910d570142
Carlos Estrada <trokotech@gmail.com>
2020-11-06 23:06:22 +0100

    back to green

--- themes/MyDefault.js
+++ themes/MyDefault.js
@@ -1,4 +1,4 @@
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#000000");
-term_set('cursor-color', 'rgba(255, 255, 255, 0.4)');
+term_set('cursor-color', 'rgba(63, 222, 233, 0.5)');
 term_set('cursor-blink', true);
