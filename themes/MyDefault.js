576394bd2913d941145b3e196ebe9fc6a76fe339
Carlos Estrada <carlos@mambrino.iterando.net>
2021-04-22 19:19:57 +0200

    cursor alpha

--- themes/MyDefault.js
+++ themes/MyDefault.js
@@ -1,4 +1,4 @@
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#000000");
-term_set('cursor-color', 'rgba(0, 255, 0, 0)');
+term_set('cursor-color', 'rgba(0, 255, 0, 1)');
 term_set('cursor-blink', true);
