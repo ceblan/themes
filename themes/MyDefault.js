385e184e506f5bd435052bfac6dc3d83e83f010e
Carlos Estrada <carlos@mambrino.iterando.net>
2021-04-22 19:47:05 +0200

    blink++

--- themes/MyDefault.js
+++ themes/MyDefault.js
@@ -1,4 +1,4 @@
 t.prefs_.set('foreground-color', "#ffffff");
 t.prefs_.set('background-color', "#000000");
-t.prefs_.set('cursor-color', 'rgba(0, 255, 0, 0.5)');
 t.prefs_.set('cursor-blink', true);
+t.prefs_.set('cursor-color', 'rgba(0, 255, 0, 0.5)');
